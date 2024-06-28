c7b06463e3981703787aac023c9abf25e2f9420b
Jason Roberts <jason@vastpuddle.com.au>
2024-06-28 11:22:40 +1000

    chore: Update package.json and package-lock.json to include dotenv v16.4.5

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,45 +4,79 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Searching for a student',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    link: '/docs/general#searching-for-a-student',
     description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
+      <a href="/docs/general#searching-for-a-student">
+        JUNIPA keeps your information available to you, securely, at all times. Finding a student quickly is important.
+      </a>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Add adjustment evidence as a Teacher',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    link: '/docs/general#add-adjustment-evidence-as-a-teacher',
     description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
+      <a href="/docs/general#add-adjustment-evidence-as-a-teacher">
+        Junipa leverages your school timetable to display what students you are interacting with today.
+      </a>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Add adjustment evidence as a Case Manager',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    link: '/docs/general#add-adjustment-evidence-as-a-case-manager',
     description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
+      <a href="/docs/general#add-adjustment-evidence-as-a-case-manager">
+        Junipa leverages your case load, displaying a list of those students you frequently interact with.
+      </a>
+    ),
+  },
+  {
+    title: 'Attach files as part of adjustment evidence',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    link: '/docs/general#attach-files-photos-videos-documents-as-part-of-adjustment-evidence',
+    description: (
+      <a href="/docs/general#attach-files-photos-videos-documents-as-part-of-adjustment-evidence">
+        Junipa lets you upload evidence straight onto a student's record as part of the Adding NCCD adjustment workflow.
+      </a>
+    ),
+  },
+  {
+    title: 'Assign an adjustment level to a student',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    link: '/docs/general#assign-an-adjustment-level-to-a-student',
+    description: (
+      <a href="/docs/general#assign-an-adjustment-level-to-a-student">
+        For a student to be eligible for an adjustment level they must be enrolled through Junipa.
+      </a>
+    ),
+  },
+  {
+    title: 'Change a student\'s adjustment level',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    link: '/docs/general#change-a-students-adjustment-level',
+    description: (
+      <a href="/docs/general#change-a-students-adjustment-level">
+        For a student to be eligible for an adjustment level they must be enrolled through Junipa.
+      </a>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({Svg, title, description, link}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <a href={link}>
+          <Svg className={styles.featureSvg} role="img" />
+        </a>
       </div>
       <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
+        <Heading as="h3">
+          <a href={link}>{title}</a>
+        </Heading>
         <p>{description}</p>
       </div>
     </div>
